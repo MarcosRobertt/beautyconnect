2b4ef0bebdc9163ba75395e550d7bad422306bdc
Marcos Robert <ro.berte@hotmail.com>
2026-08-22 14:49:02 -0300

    feat: adiciona credenciais do firebase web

---
+++ lib/firebase_options.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {
+  static const FirebaseOptions currentPlatform = FirebaseOptions(
+    apiKey: "AIzaSyBwC863dTIaAgRgvU-BMRDhHOOHt3xmOeE",
+    authDomain: "beautyconnect-teste.firebaseapp.com",
+    projectId: "beautyconnect-teste",
+    storageBucket: "beautyconnect-teste.firebasestorage.app",
+    messagingSenderId: "1034270470948",
+    appId: "1:1034270470948:web:b1bbd5026d9a4e63ef79b1",
+    measurementId: "G-LLJRLGCYS8",
+  );
+}
