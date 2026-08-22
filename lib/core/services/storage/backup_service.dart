6d21385ec440df3f6418ebb34e4ade0d0589df85
Marcos Robert <ro.berte@hotmail.com>
2026-08-22 20:02:45 -0300

    Implement backup and restore functionality in BackupService

--- lib/core/services/storage/backup_service.dart
+++ lib/core/services/storage/backup_service.dart
@@ -1,4 +1,38 @@
-Future<Map<String, dynamic>?> importar() async {
+import 'dart:convert';
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+
+import '../../../features/agenda/models/agendamento.dart';
+import '../../../features/clientes/models/cliente.dart';
+import '../../../features/servicos/models/servico.dart';
+
+class BackupService {
+  Future<void> exportar({
+    required List<Cliente> clientes,
+    required List<Agendamento> agendamentos,
+    List<Servico> servicos = const [],
+  }) async {
+    final mapaBackup = {
+      'versao': 2,
+      'dataCriacao': DateTime.now().toIso8601String(),
+      'clientes': clientes.map((c) => c.toJson()).toList(),
+      'agendamentos': agendamentos.map((a) => a.toJson()).toList(),
+      'servicos': servicos.map((s) => s.toJson()).toList(),
+    };
+
+    final jsonString = const JsonEncoder.withIndent('  ').convert(mapaBackup);
+    final bytes = utf8.encode(jsonString);
+    final blob = html.Blob([bytes], 'application/json');
+    final url = html.Url.createObjectUrlFromBlob(blob);
+
+    final anchor = html.AnchorElement(href: url)
+      ..setAttribute('download', 'backup_beautyconnect_${DateTime.now().millisecondsSinceEpoch}.json')
+      ..click();
+
+    html.Url.revokeObjectUrl(url);
+  }
+
+  Future<Map<String, dynamic>?> importar() async {
     final uploadInput = html.FileUploadInputElement()..accept = '.json';
     uploadInput.click();
 
@@ -22,3 +56,41 @@ Future<Map<String, dynamic>?> importar() async {
 
     return processarBackupJson(conteudoJson);
   }
+
+  static Map<String, dynamic> processarBackupJson(String conteudoJson) {
+    final Map<String, dynamic> dados = jsonDecode(conteudoJson) as Map<String, dynamic>;
+
+    final List<Cliente> clientes = [];
+    if (dados['clientes'] != null) {
+      for (final item in (dados['clientes'] as List)) {
+        try {
+          clientes.add(Cliente.fromJson(Map<String, dynamic>.from(item as Map)));
+        } catch (_) {}
+      }
+    }
+
+    final List<Agendamento> agendamentos = [];
+    if (dados['agendamentos'] != null) {
+      for (final item in (dados['agendamentos'] as List)) {
+        try {
+          agendamentos.add(Agendamento.fromJson(Map<String, dynamic>.from(item as Map)));
+        } catch (_) {}
+      }
+    }
+
+    final List<Servico> servicos = [];
+    if (dados['servicos'] != null) {
+      for (final item in (dados['servicos'] as List)) {
+        try {
+          servicos.add(Servico.fromJson(Map<String, dynamic>.from(item as Map)));
+        } catch (_) {}
+      }
+    }
+
+    return {
+      'clientes': clientes,
+      'agendamentos': agendamentos,
+      'servicos': servicos,
+    };
+  }
+}
