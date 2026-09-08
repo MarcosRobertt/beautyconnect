7ca08f446485fb135cc1762eb61afe6f818858df
Marcos Robert <ro.berte@hotmail.com>
2026-09-08 20:45:02 -0300

    Fix import path for firebase_auth package

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:firebase_auth/package:firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
